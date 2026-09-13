6da78cecde77a8acf8923aafdc9138bc9faf70c0
MyBets <jp_veiculos@icloud.com>
2026-09-13 15:56:13 -0300

    Remove bloqueio de login do lobby

--- backend/frontend/games-auth.js
+++
@@ -1,8 +0,0 @@
-(()=>{
-  const user=localStorage.getItem('jpbet_user');
-  if(user)return;
-  const params=new URLSearchParams(location.search);
-  const game=params.get('game');
-  const destino=game?`/?login=1&game=${encodeURIComponent(game)}`:'/?login=1';
-  location.replace(destino);
-})();
