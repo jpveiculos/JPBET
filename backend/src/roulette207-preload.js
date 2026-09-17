20eb1a158f32a5c1ddce0624a77051851af7fb76
MyBets <jp_veiculos@icloud.com>
2026-09-17 10:48:36 -0300

    Update roulette207-preload.js

--- backend/src/roulette207-preload.js
+++ backend/src/roulette207-preload.js
@@ -1,8 +1,25 @@
 import express from "express";
 import roulette207Router from "./roulette207.js";
+import myDragonRouter from "./myDragon.js";
 
-const originalListen = express.application.listen;
-express.application.listen = function (...args) {
-  this.use("/api/roulette207", roulette207Router);
-  return originalListen.apply(this, args);
-};
+const originalListen =
+  express.application.listen;
+
+express.application.listen =
+  function (...args) {
+
+    this.use(
+      "/api/roulette207",
+      roulette207Router
+    );
+
+    this.use(
+      "/api/my-dragon",
+      myDragonRouter
+    );
+
+    return originalListen.apply(
+      this,
+      args
+    );
+  };
